c31cf79a6a711686b63ec1e898fdab32d90d6e79
Bastien Durand <basilexpeditions@gmail.com>
2023-08-29 08:32:26 +0200

    added endpoints for Create one, Get all, Get by id, Update by id & Delete by id

--- server.js
+++ server.js
@@ -22,17 +22,68 @@ app.post("/createtodo", async (req, res) => {
     );
     res.json(newTodo.rows[0]);
   } catch (error) {
-    console.log(error);
+    console.error(error.message);
   }
 });
 
 // Get all todo
 
+app.get("/alltodo", async (req, res) => {
+  try {
+    const getAllTodo = await pool.query("SELECT * FROM todo");
+    res.send(getAllTodo.rows);
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+
 // Get a todo
+
+app.get("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const todoId = await pool.query("SELECT * FROM todo WHERE todo_id=$1", [
+      id,
+    ]);
+    if (todoId.rows < 1) {
+      res.send(`Todo with id: '${id}' not found`);
+    } else {
+      res.send(todoId.rows);
+    }
+  } catch (error) {
+    console.error(error.message);
+  }
+});
 
 // Update a todo
 
+app.put("/todos/:id", async (req, res) => {
+  const { id } = req.params;
+  const { description } = req.body;
+  try {
+    const updatedDescription = await pool.query(
+      "UPDATE todo SET description = $1 WHERE todo_id = $2",
+      [description, id]
+    );
+    res.json("Todo was updated");
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+
 // Delete a todo
+
+app.delete("/todos/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const deleteById = await pool.query("DELETE FROM todo WHERE todo_id = $1", [
+      id,
+    ]);
+    res.json("Item deleted");
+  } catch (error) {
+    console.error(error.message);
+  }
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
